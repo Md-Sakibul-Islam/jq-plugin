a64c21607e85217a8ace472e85818dc3b06b4e33
Md-Sakibul-Islam <sakibul.diu@gmail.com>
2024-08-29 00:50:45 +0600

    slick slider done

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+  $(".slick-slider").slick({
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    centerMode: true,
+    variableWidth: true,
+    nextArrow:
+      '<button class="slider-next"><i class="fa-solid fa-arrow-left"></i></button>',
+    prevArrow:
+      '<button class="slider-prev"><i class="fa-solid fa-arrow-right"></i></button>',
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          centerMode: true,
+          variableWidth: true,
+        },
+      },
+
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          centerMode: true,
+          variableWidth: true,
+        },
+      },
+
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          nextArrow: "",
+          prevArrow: "",
+
+        },
+      },
+    ],
+  });
+});
